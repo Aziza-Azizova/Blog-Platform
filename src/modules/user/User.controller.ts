e801e541d494e8ce1466760a66f72ff0a7d87b7a
Aziza Azizova <aziza.azizova01@gmail.com>
2024-10-08 17:28:22 +0500

    fix:all comments and feedbacks resolved

--- src/modules/user/User.controller.ts
+++ src/modules/user/User.controller.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { userLoginDtoSchema, userSignupDtoSchema } from "./dto/user-auth-dto";
-import { UserServices } from "./User.service";
+import { userLoginDtoSchema, userSignupDtoSchema } from "./dto/user-auth.dto";
+import { UserService } from "./User.service";
 import { validateAuth } from "../../shared/validators/auth.validator";
 
 
@@ -9,11 +9,11 @@ export const UserController = Router();
 UserController.post(
     '/signup',
     validateAuth(userSignupDtoSchema),
-    UserServices.registration
+    UserService.register
 );
 
 UserController.post(
     '/login',
     validateAuth(userLoginDtoSchema),
-    UserServices.login
+    UserService.login
 );
